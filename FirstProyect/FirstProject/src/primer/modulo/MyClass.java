d8577211d270012fe5ce55a80aa7d330c4b2404b
elHenryettas <92875883+elHenryettas@users.noreply.github.com>
2022-06-25 19:03:53 -0300

    Salute!

---
+++ FirstProyect/FirstProject/src/primer/modulo/MyClass.java
@@ -0,0 +1,10 @@
+package primer.modulo;
+
+public class MyClass {
+    int x = 4;
+    String hola = "yo te saludo!";
+    public static void main(String[] args) {
+        MyClass myObj = new MyClass();
+        System.out.println(myObj.hola);
+    }
+}
