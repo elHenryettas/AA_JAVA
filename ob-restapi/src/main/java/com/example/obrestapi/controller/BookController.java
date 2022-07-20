264021b2c620083c0a58cde3b1b887efb9d9645e
elHenryettas <92875883+elHenryettas@users.noreply.github.com>
2022-07-20 16:03:48 -0300

    .

--- ob-restapi/src/main/java/com/example/obrestapi/controller/BookController.java
+++ ob-restapi/src/main/java/com/example/obrestapi/controller/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -59,7 +60,16 @@ public class BookController {
        return bookRepository.save(body);
     }
 
+    /* @PutMapping("/api/update")
+    public ResponseEntity<Book> updateById(@PathVariable Long id, @RequestBody Book body){
+        Optional<Book> bookOpt = bookRepository.findById(id);
+        if(bookOpt.isPresent())
+            Book book = bookOpt.get();
 
+            System.out.println(body); 
+            return ResponseEntity.ok(body);
+            
+    }  */
 
 
     /* public String updateBook(){
